042b4fa7dab8e725d1c3cd97cba6b79b1e073ea0
unknown <pa4itsky.jaroslav@gmail.com>
2023-04-07 21:15:53 +0300

    2 программа

--- seminar__1/Program.cs
+++ seminar__1/Program.cs
@@ -1,5 +1,19 @@
 ﻿// Нахождение квадрата числа
-Console.WriteLine ("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
-number = number*number;
-Console.WriteLine(number);
+//Console.WriteLine ("Введите число");
+//int number = Convert.ToInt32(Console.ReadLine());
+//number = number*number;
+//number*=number
+//Console.WriteLine(number);
+
+Console.WriteLine ("Введите первое число");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите втрое число");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+if (firstnumber == secondnumber*secondnumber)
+{
+     Console.WriteLine ("Число один является квадратом числа два");  
+}
+else
+{
+    Console.WriteLine ("Число один не является квадратом чисда два");
+}
